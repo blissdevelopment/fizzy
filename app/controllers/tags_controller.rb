53b61c0752fa18e2686068e6573c99fff03cc9a4
Jeffrey Hardy <jeff@37signals.com>
2024-10-01 16:25:31 -0400

    Inline tag finding

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,6 @@ class TagsController < ApplicationController
   include BucketScoped
 
   before_action :set_bubble, only: %i[ new create ]
-  before_action :set_tag, only: :destroy
 
   def index
     @tags = Current.account.tags.order(:title)
@@ -17,17 +16,13 @@ class TagsController < ApplicationController
   end
 
   def destroy
-    @tag.destroy
+    Current.account.tags.find(params[:id]).destroy
     redirect_to bucket_tags_url(@bucket)
   end
 
   private
     def tag_params
       params.require(:tag).permit(:title)
-    end
-
-    def set_tag
-      @tag = Current.account.tags.find(params[:id])
     end
 
     def set_bubble
